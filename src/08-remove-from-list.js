e4df9572e00929582b47fa0eaad744de95b07e34
Masterofonion <maltsevslava@inbox.ru>
2021-04-07 19:56:44 +0300

    feat: add some solutions

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,17 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  while (l.value === k) {
+    l = l.next; // eslint-disable-line no-param-reassign
+  }
+  let node = l;
+  while (node.next) {
+    if (node.next.value === k) {
+      node.next = node.next.next;
+    }
+    node = node.next;
+  }
+  return l;
 }
-
 module.exports = removeKFromList;
