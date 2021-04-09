19b524d52ead8de9ae0aa3e8daa949690acc4f6f
Masterofonion <maltsevslava@inbox.ru>
2021-04-09 05:09:18 +0300

    feat: add solution for task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,34 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const res = [];
+  const array = str.split('');
+  array.reduce((accum, letter, index) => {
+    if (accum.includes(letter) || accum.length === 0) {
+      accum.push(letter);
+      console.log(accum);
+      if (index === (array.length - 1)) {
+        if (accum.length > 1) {
+          res.push(`${accum.length}${accum[0]}`);
+        } else {
+          res.push(`${accum[0]}`);
+        }
+      }
+    } else {
+      if (accum.length > 1) {
+        res.push(`${accum.length}${accum[0]}`);
+      } else {
+        res.push(`${accum[0]}`);
+      }
+      accum = [];// eslint-disable-line no-param-reassign
+      accum.push(letter);
+      if (index === (array.length - 1)) {
+        res.push(`${accum[0]}`);
+      }
+    }
+    return accum;
+  }, []);
+  return res.join('');
 }
-
 module.exports = encodeLine;
